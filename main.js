2852ccef33c62d54a872b4fccec7787498ef6504
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-13 17:40:33 -0700

    Add: Add details to init and Game construcures.

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
 var Sudoku = ( function ( $ ) {
 	var _instance, _game, defaultConfig = {};
 
-	var init = function () {
-
+	var init = function ( config ) {
+		conf = $.extend( {}, defaultConfig, config);
+		_game = new Game( conf );
+		return {
+			//Return a visual representation of the board
+			gameBoard: function () {
+				return _game.buildBoard();
+			},
+			reset: function () {
+				_game.clearBoard();
+			},
+			solution: function (){
+				return _game.getSolution();
+			}
+		}
 	};
 
+	//Game initialization and logic
+	var Game = function () {
+
+	};
+	Game.prototype.buildBoard = function () {
+		//TODO
+		return board;
+	};
+	Game.prototype.reset = function () {
+		//TODO
+	};
+	Game.prototype.solution = function () {
+		//TODO
+		return solution;
+	};
+
+	//Singleton public methods
+	return {
+		//get the Singleton instance
+		getInstance: function ( config ) {
+			if (!_instance) {
+				_instance = init( config );
+			}
+			return _instance;
+		}
+	};
 })( jQuery );
