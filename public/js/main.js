ea606e2417be6ff7a3ecb83b663123c8352e59a2
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-13 22:52:34 -0700

    Mod: Add parentMatrix and childMatrix.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 var Sudoku = ( function ( $ ) {
 	var _instance, _game, defaultConfig = {};
+	var createNineCellsMatrix;
 
 	var init = function ( config ) {
 		conf = $.extend( {}, defaultConfig, config);
@@ -7,6 +8,7 @@ var Sudoku = ( function ( $ ) {
 		return {
 			//Return a visual representation of the board
 			gameBoard: function () {
+				console.log("here");
 				return _game.buildBoard();
 			},
 			reset: function () {
@@ -23,8 +25,19 @@ var Sudoku = ( function ( $ ) {
 
 	};
 	Game.prototype.buildBoard = function () {
-		//TODO
-		return board;
+		var gameBoard, $parentMatrix, $tr, $td;
+		//Create parentMatrix and childMatrix
+		$parentMatrix = $( '<table>' ).addClass('parentMatrix');
+		$childMatrix = $( '<table>' ).addClass('childMatrix');
+
+		//Generate Cells of Matrix
+		createNineCellsMatrix($childMatrix, $tr, $td);
+		createNineCellsMatrix($parentMatrix, $tr, $td, $childMatrix);
+
+		$childMatrixCell = $( '<input>' );
+
+		gameBoard = $parentMatrix;
+		return gameBoard;
 	};
 	Game.prototype.reset = function () {
 		//TODO
@@ -34,11 +47,26 @@ var Sudoku = ( function ( $ ) {
 		return solution;
 	};
 
+	//Helper functions
+	createNineCellsMatrix = function (parentMatrix, $tr, $td, childMatrix) {
+			for (var i = 0; i < 3; i++) {
+				// if (i = 1) {debugger};
+				$tr = $( '<tr>' );
+				parentMatrix.append($tr);
+				for (var j = 0; j < 3; j++) {
+					if (childMatrix) {
+						$td = $( '<td>' ).append( childMatrix );
+					};
+					$tr.append($td);
+				};
+			};
+		};
 	//Singleton public methods
 	return {
 		//get the Singleton instance
 		getInstance: function ( config ) {
 			if (!_instance) {
+			console.log('get instance');
 				_instance = init( config );
 			}
 			return _instance;
