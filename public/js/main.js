d374f961573251c49b614a9762045d9d8fc589cb
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-13 22:53:35 -0700

    Rea: Remove createNineMatrix function at main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 var Sudoku = ( function ( $ ) {
 	var _instance, _game, defaultConfig = {};
-	var createNineCellsMatrix;
 
 	var init = function ( config ) {
 		conf = $.extend( {}, defaultConfig, config);
@@ -25,18 +24,6 @@ var Sudoku = ( function ( $ ) {
 
 	};
 	Game.prototype.buildBoard = function () {
-		var gameBoard, $parentMatrix, $tr, $td;
-		//Create parentMatrix and childMatrix
-		$parentMatrix = $( '<table>' ).addClass('parentMatrix');
-		$childMatrix = $( '<table>' ).addClass('childMatrix');
-
-		//Generate Cells of Matrix
-		createNineCellsMatrix($childMatrix, $tr, $td);
-		createNineCellsMatrix($parentMatrix, $tr, $td, $childMatrix);
-
-		$childMatrixCell = $( '<input>' );
-
-		gameBoard = $parentMatrix;
 		return gameBoard;
 	};
 	Game.prototype.reset = function () {
@@ -47,20 +34,6 @@ var Sudoku = ( function ( $ ) {
 		return solution;
 	};
 
-	//Helper functions
-	createNineCellsMatrix = function (parentMatrix, $tr, $td, childMatrix) {
-			for (var i = 0; i < 3; i++) {
-				// if (i = 1) {debugger};
-				$tr = $( '<tr>' );
-				parentMatrix.append($tr);
-				for (var j = 0; j < 3; j++) {
-					if (childMatrix) {
-						$td = $( '<td>' ).append( childMatrix );
-					};
-					$tr.append($td);
-				};
-			};
-		};
 	//Singleton public methods
 	return {
 		//get the Singleton instance
