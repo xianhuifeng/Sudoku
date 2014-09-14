75e26e52ada1a36e833f2ee03237ef272d43053f
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-14 00:17:58 -0700

    Add: Add table to gameBoardContainer when refresh the page.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
-var Sudoku = ( function ( $ ) {
+var Sudoku = ( function ($) {
 	var _instance, _game, defaultConfig = {};
 
-	var init = function ( config ) {
+	var init = function (config) {
 		conf = $.extend( {}, defaultConfig, config);
 		_game = new Game( conf );
 		return {
@@ -21,10 +21,38 @@ var Sudoku = ( function ( $ ) {
 
 	//Game initialization and logic
 	var Game = function () {
-
+		this.$cellMatrix = {};
 	};
 	Game.prototype.buildBoard = function () {
-		return gameBoard;
+		//make table
+		var N = 9;
+		var $tr, $td;
+		var $table = $('<table>');
+			//make 9 rows
+			for (var i = 0; i < N ; i++) {
+				$tr = $('<tr>');
+				this.$cellMatrix[i] = {};
+				//for each row make 9 input 
+				for (var j = 0; j < N ; j++) {
+					this.$cellMatrix[i][j] = $('<input>')
+													.attr('maxlength', 1)
+													.data('row', i)
+													.data('col', j)
+													.on('keyup', $.proxy(this.onKeyUp, this));
+					$td = $('<td>').append(this.$cellMatrix[i][j]);
+					//$td = $('<td>').append('<p>funny</p>');
+					//check the section of the input
+
+						//if section even
+							//addClass sectionEven
+						//else
+							//addClass sectionOdd
+					$tr.append($td);
+				};
+				console.log($tr);
+				$table.append($tr);
+			};
+		return $table;
 	};
 	Game.prototype.reset = function () {
 		//TODO
