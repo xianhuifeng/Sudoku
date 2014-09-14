89a3ee0937f9fb50614ff799a21cc258ce6aa4a7
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-13 19:15:16 -0700

    Mod: Add folders for modularity.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-console.log(__dirname);
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
