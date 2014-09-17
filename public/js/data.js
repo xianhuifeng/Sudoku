4cfab3a359de52da0bcf95639221d4339a579acb
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-16 18:42:03 -0700

    Mod: Clean up code.

--- public/js/data.js
+++ public/js/data.js
@@ -1,104 +1,96 @@
-var DummyBoardInputValsGenerator = function (cellMatrix) {
-	function setVal(i, j, value) {
-		cellMatrix[i][j].val(value).attr('disabled', true);
-	}; 
-	setVal(0,0,5);
-	setVal(0,1,3);
-	setVal(0,4,7);
-	setVal(1,0,6);
-	setVal(1,3,1);
-	setVal(1,4,9);
-	setVal(1,5,5);
-	setVal(2,1,9);
-	setVal(2,2,8);
-	setVal(2,7,6);
-	setVal(3,0,8);
-	setVal(3,4,6);
-	setVal(3,8,3);
-	setVal(4,0,4);
-	setVal(4,3,8);
-	setVal(4,5,3);
-	setVal(4,8,1);
-	setVal(5,0,7);
-	setVal(5,4,2);
-	setVal(5,8,6);
-	setVal(6,1,6);
-	setVal(6,6,2);
-	setVal(6,7,8);
-	setVal(7,3,4);
-	setVal(7,4,1);
-	setVal(7,5,9);
-	setVal(7,8,5);
-	setVal(8,4,8);
-	setVal(8,7,7);
-	setVal(8,8,9);
+var DummyBoardInputValsGenerator = function(cellMatrix) {
+    function setVal(i, j, value) {
+        cellMatrix[i][j].val(value).attr('disabled', true);
+    };
+    setVal(0, 0, 5);
+    setVal(0, 1, 3);
+    setVal(0, 4, 7);
+    setVal(1, 0, 6);
+    setVal(1, 3, 1);
+    setVal(1, 4, 9);
+    setVal(1, 5, 5);
+    setVal(2, 1, 9);
+    setVal(2, 2, 8);
+    setVal(2, 7, 6);
+    setVal(3, 0, 8);
+    setVal(3, 4, 6);
+    setVal(3, 8, 3);
+    setVal(4, 0, 4);
+    setVal(4, 3, 8);
+    setVal(4, 5, 3);
+    setVal(4, 8, 1);
+    setVal(5, 0, 7);
+    setVal(5, 4, 2);
+    setVal(5, 8, 6);
+    setVal(6, 1, 6);
+    setVal(6, 6, 2);
+    setVal(6, 7, 8);
+    setVal(7, 3, 4);
+    setVal(7, 4, 1);
+    setVal(7, 5, 9);
+    setVal(7, 8, 5);
+    setVal(8, 4, 8);
+    setVal(8, 7, 7);
+    setVal(8, 8, 9);
 };
 
-var getSolutionData = function (cellMatrix) {
-	function setSolutionVal(i, j, value) {
-		//Set solution val
-		//Make solutijon input disabled true and dirty true so that user cannot 
-		//modify them and when user click restart we set will then back to disabled
-		//false, dirty false and val('')
-		cellMatrix[i][j].val(value).attr('disabled', true).attr('dirty', true);
-	};
-	setSolutionVal(0,2,4);
-	setSolutionVal(0,3,6);
-	setSolutionVal(0,5,8);
-	setSolutionVal(0,6,9);
-	setSolutionVal(0,7,1);
-	setSolutionVal(0,8,2);
-	setSolutionVal(1,1,7);
-	setSolutionVal(1,2,2);
-	setSolutionVal(1,6,3);
-	setSolutionVal(1,7,4);
-	setSolutionVal(1,8,8);
-	setSolutionVal(2,0,1);
-	setSolutionVal(2,3,3);
-	setSolutionVal(2,4,4);
-	setSolutionVal(2,5,2);
-	setSolutionVal(2,6,5);
-	setSolutionVal(2,8,7);
-	setSolutionVal(3,1,5);
-	setSolutionVal(3,2,9);
-	setSolutionVal(3,3,7);
-	setSolutionVal(3,5,1);
-	setSolutionVal(3,6,4);
-	setSolutionVal(3,7,2);
-	setSolutionVal(4,1,2),
-	setSolutionVal(4,2,6);
-	setSolutionVal(4,4,5);
-	setSolutionVal(4,6,7);
-	setSolutionVal(4,7,9);
-	setSolutionVal(5,1,1);
-	setSolutionVal(5,2,3);
-	setSolutionVal(5,3,9);
-	setSolutionVal(5,5,4);
-	setSolutionVal(5,6,8);
-	setSolutionVal(5,7,5);
-	setSolutionVal(6,0,9);
-	setSolutionVal(6,2,1);
-	setSolutionVal(6,3,5);
-	setSolutionVal(6,4,3);
-	setSolutionVal(6,5,7);
-	setSolutionVal(6,8,4);
-	setSolutionVal(7,0,2);
-	setSolutionVal(7,1,8);
-	setSolutionVal(7,2,7);
-	setSolutionVal(7,6,6);
-	setSolutionVal(7,7,3);
-	setSolutionVal(8,0,3);
-	setSolutionVal(8,1,4);
-	setSolutionVal(8,2,5);
-	setSolutionVal(8,3,2);
-	setSolutionVal(8,5,6);
-	setSolutionVal(8,6,1);
+var getSolutionData = function(cellMatrix) {
+    function setSolutionVal(i, j, value) {
+        //Set solution val
+        //Make solutijon input disabled true and dirty true so that user cannot 
+        //modify them and when user click restart we set will then back to disabled
+        //false, dirty false and val('')
+        cellMatrix[i][j].val(value).attr('disabled', true).attr('dirty', true);
+    };
+    setSolutionVal(0, 2, 4);
+    setSolutionVal(0, 3, 6);
+    setSolutionVal(0, 5, 8);
+    setSolutionVal(0, 6, 9);
+    setSolutionVal(0, 7, 1);
+    setSolutionVal(0, 8, 2);
+    setSolutionVal(1, 1, 7);
+    setSolutionVal(1, 2, 2);
+    setSolutionVal(1, 6, 3);
+    setSolutionVal(1, 7, 4);
+    setSolutionVal(1, 8, 8);
+    setSolutionVal(2, 0, 1);
+    setSolutionVal(2, 3, 3);
+    setSolutionVal(2, 4, 4);
+    setSolutionVal(2, 5, 2);
+    setSolutionVal(2, 6, 5);
+    setSolutionVal(2, 8, 7);
+    setSolutionVal(3, 1, 5);
+    setSolutionVal(3, 2, 9);
+    setSolutionVal(3, 3, 7);
+    setSolutionVal(3, 5, 1);
+    setSolutionVal(3, 6, 4);
+    setSolutionVal(3, 7, 2);
+    setSolutionVal(4, 1, 2),
+    setSolutionVal(4, 2, 6);
+    setSolutionVal(4, 4, 5);
+    setSolutionVal(4, 6, 7);
+    setSolutionVal(4, 7, 9);
+    setSolutionVal(5, 1, 1);
+    setSolutionVal(5, 2, 3);
+    setSolutionVal(5, 3, 9);
+    setSolutionVal(5, 5, 4);
+    setSolutionVal(5, 6, 8);
+    setSolutionVal(5, 7, 5);
+    setSolutionVal(6, 0, 9);
+    setSolutionVal(6, 2, 1);
+    setSolutionVal(6, 3, 5);
+    setSolutionVal(6, 4, 3);
+    setSolutionVal(6, 5, 7);
+    setSolutionVal(6, 8, 4);
+    setSolutionVal(7, 0, 2);
+    setSolutionVal(7, 1, 8);
+    setSolutionVal(7, 2, 7);
+    setSolutionVal(7, 6, 6);
+    setSolutionVal(7, 7, 3);
+    setSolutionVal(8, 0, 3);
+    setSolutionVal(8, 1, 4);
+    setSolutionVal(8, 2, 5);
+    setSolutionVal(8, 3, 2);
+    setSolutionVal(8, 5, 6);
+    setSolutionVal(8, 6, 1);
 };
-
-
-
-
-
-
-
-
--- public/js/main.js
+++ public/js/main.js
@@ -1,175 +1,192 @@
-var Sudoku = ( function ($) {
-	var _instance, _game, defaultConfig = {};
+var Sudoku = (function($) {
+    var _instance, _game = {};
 
-	var init = function (config) {
-		conf = $.extend( {}, defaultConfig, config);
-		_game = new Game( conf );
-		return {
-			gameBoard: function () {
-				return _game.buildBoard();
-			},
-			reset: function () {
-				_game.clearBoard();
-			},
-			solution: function (){
-				_game.getSolution();
-			}
-		}
-	};
+    var init = function() {
+        _game = new Game();
+        return {
+            gameBoard: function() {
+                return _game.buildBoard();
+            },
+            reset: function() {
+                _game.clearBoard();
+            },
+            solution: function() {
+                _game.getSolution();
+            }
+        }
+    };
 
-	//Game initialization and logic.
-	var Game = function () {
-		this.$cellMatrix = {};
-		this.N = 9;
-	};
-	Game.prototype.buildBoard = function () {
-		var $tr, $td, sectIDi, sectIDj;
+    //Game initialization and logic.
+    var Game = function() {
+        this.$cellMatrix = {};
+        this.N = 9;
+    };
+    Game.prototype.buildBoard = function() {
+        var $tr, $td, sectIDi, sectIDj;
 
-		//Make a new table and add class style.
-		var $table = $('<table>').addClass('parent-matrix');
+        //Make a new table and add class style.
+        var $table = $('<table>').addClass('parent-matrix');
 
-			//Make nine rows for this table
-			for (var i = 0; i < this.N ; i++) {
-				$tr = $('<tr>');
-				this.$cellMatrix[i] = {};
+        //Make nine rows for this table
+        for (var i = 0; i < this.N; i++) {
+            $tr = $('<tr>');
+            this.$cellMatrix[i] = {};
 
-				//For each row, we make nine table data and we append input to it.
-				for (var j = 0; j < this.N ; j++) {
-					var self = this;
-					self.$cellMatrix[i][j] = $('<input>')
-													.addClass('input-style')
-													.attr('maxlength', 1)
-													.attr('dirty', false) //This attr is used for clearBoard().
-													.data('row', i)
-													.data('col', j)
-													.keyup(function (e) {
-														//This function takes there parameters. The first argument gets injected 
-														//is the event; the second is the current content which related to the event keyup;
-														//and the third argumet is the object Game itself.
-														self.onKeyUp(e, this, self);
-													});
+            //For each row, we make nine table data and we append input to it.
+            for (var j = 0; j < this.N; j++) {
+                var self = this;
 
-					$td = $('<td>').append(this.$cellMatrix[i][j]);
-					//Check the section of the input so we can set different class for each section
-					//and also we will use this section for validation.
-					sectIDi = Math.floor( i / 3 );
-					sectIDj = Math.floor( j / 3 );
+                self.$cellMatrix[i][j] = $('<input>')
+                    .addClass('input-style')
+                    .attr('maxlength', 1)
+                    .attr('dirty', false) //This attr is used for clearBoard().
+                		.data('row', i)
+                    .data('col', j)
+                    .keyup(function(e) {
 
-					if ( ( sectIDi + sectIDj ) % 2 === 0 ) {
-						$td.addClass( 'section-even' );
-					} else {
-						$td.addClass( 'section-odd' );
-					}
-					$tr.append($td);
-				};
-				$table.append($tr);
-			};
+                        //This function takes there parameters. The first argument gets injected 
+                        //is the event; the second is the current content which related to the event keyup;
+                        //and the third argumet is the object Game itself.
 
-		/*
-		*This can be changed after generator function is implemented, 
-		*however now we will only have one board.
-		*In future, we will add random fucntion for BoardInputValsGenerator
-		*/
-		DummyBoardInputValsGenerator(this.$cellMatrix);
-		return $table;
-	};
-	Game.prototype.clearBoard = function () {
-		for (var i = 0; i < this.N; i++) {
-			for (var j = 0; j < this.N; j++) {
+                        self.onKeyUp(e, this, self);
+                    });
 
-				/*Check if the input attr is dirty or not, if is, clear val and set to false
-				*In this case, both user input and solution data can make attr dirty true	
-				*/
-				if(this.$cellMatrix[i][j].attr('dirty') === "true"){ // Here need to consider true as a string
-					this.$cellMatrix[i][j].val('');
-					this.$cellMatrix[i][j].css('background-color', 'transparent');
-					/*This takes care of solution data value and user input value both
-					*If it was user input value, only dirty set back to false
-					*If it was solution data value, set dirty back to false and set disabled back to false
-					*/
-					this.$cellMatrix[i][j].attr('dirty', false).attr('disabled', false);
-				}
-			};
-		};	
-	};
-	Game.prototype.getSolution = function () {
-		//Call method clearBoard() to set back attr dirty, disabled, and val()
-		this.clearBoard();
-		getSolutionData(this.$cellMatrix);
-	};
+                $td = $('<td>').append(this.$cellMatrix[i][j]);
 
-	//This validation is not implemented yet,
-	//it still needs to check odd and even parts. Finish it
-	//later.
-	Game.prototype.inputValidate = function (cellMatrix, value, data) {
-		console.log(value, data);
-		if(value === ""){
-			return true;
-		}
-		var validData = true;
-		var row = data.row;
-		var col = data.col;
+                //Check the section of the input so we can set different class for each section
+                //and also we will use this section for validation.
 
-		//Check data validation by row.
-		for (var i = 0; i < this.N ; i++) {
-			if(i !== col && cellMatrix[row][i].val() === value){
-				// cellMatrix[row][i].css('background-color', 'green');
-				validData = false;
-				return validData;
-			}
-		}
-		//Check data validation by col.
-		for (var j = 0; j < this.N ; j++) {
-			if(j !== row && cellMatrix[j][col].val() === value){
-				// cellMatrix[j][col].css('background-color', 'green');
-				validData = false;
-				return validData;
-			}
-		};
-		return validData;
-	};
+                sectIDi = Math.floor(i / 3);
+                sectIDj = Math.floor(j / 3);
 
-	Game.prototype.onKeyUp = function (e,content,self) {
-		console.log('what is self', self);
-		console.log('what is content', content);
-		//if KeyCode is not 8, it means users are inserting value to input
-		//We should set dirty to true
-		//We also should check if it is validate or not
-		//if validtae, remove input-style and add input-validate-false style
-		//if not, alert for now
-		if(e.keyCode != 8){
-			$(content).attr('dirty', true); //content attr is used for clearBoard() also.	
+                if ((sectIDi + sectIDj) % 2 === 0) {
+                    $td.addClass('section-even');
+                } else {
+                    $td.addClass('section-odd');
+                }
+                $tr.append($td);
+            };
+            $table.append($tr);
+        };
 
-			//Check input validatipn
-			var isValidate = self.inputValidate(self.$cellMatrix, $(content).val(), $(content).data());
-			if(!isValidate){
-				$(content).removeClass('input-style');
-				$(content).addClass('input-validate-false');
-			} else {
-				alert('hey good guess');
-			}
-		} 
+        /*This can be changed after generator function is implemented,
+         however now we will only have one board.
+         In future, we will add random fucntion for BoardInputValsGenerator*/
+
+        DummyBoardInputValsGenerator(this.$cellMatrix);
+
+        return $table;
+    };
+    Game.prototype.clearBoard = function() {
+        for (var i = 0; i < this.N; i++) {
+            for (var j = 0; j < this.N; j++) {
+
+                /*Check if the input attr is dirty or not, if is, clear val and set to false
+                In this case, both user input and solution data can make attr dirty true*/
+
+                if (this.$cellMatrix[i][j].attr('dirty') === "true") { // Here need to consider true as a string
+                    this.$cellMatrix[i][j].val('');
+                    this.$cellMatrix[i][j].removeClass('input-validate-true');
+                    this.$cellMatrix[i][j].removeClass('input-validate-false');
+
+                    /*This takes care of solution data value and user input value both
+                     If it was user input value, only dirty set back to false
+                     If it was solution data value, set dirty back to false and set disabled back to false*/
+
+                    this.$cellMatrix[i][j].attr('dirty', false).attr('disabled', false);
+                }
+            };
+        };
+    };
+
+    Game.prototype.getSolution = function() {
+
+        //Call method clearBoard() to set back attr dirty, disabled, and val()
+        this.clearBoard();
+        getSolutionData(this.$cellMatrix);
+    };
+
+    //This validation is not implemented yet,
+    //it still needs to check odd and even parts. 
+    
+    Game.prototype.inputValidate = function(cellMatrix, value, data) {
+
+        var validData = true;
+        var row = data.row;
+        var col = data.col;
+
+        //Check if value is empty,return true
+        if (value === "") {
+            return true;
+        }
+
+        //Check if value is illegal number, return false if illegal
+        if (isNaN(value)) {
+            alert('This input needs a number from 1-9');
+            return false;
+        }
+
+        //Check data validation by row.
+        for (var i = 0; i < this.N; i++) {
+            if (i !== col && cellMatrix[row][i].val() === value) {
+                validData = false;
+                return validData;
+            }
+        }
+
+        //Check data validation by col.
+        for (var j = 0; j < this.N; j++) {
+            if (j !== row && cellMatrix[j][col].val() === value) {
+                validData = false;
+                return validData;
+            }
+        };
+
+        return validData;
+    };
+
+    Game.prototype.onKeyUp = function(e, content, self) {
+
+        /*If KeyCode is not 8, it means users are inserting value to input
+        We should set dirty to true.
+        We also should check if it is validate or not.
+        If validtae, remove input-style and add input-validate-false style.
+        And If not, add input-validate-true style.*/
+
+        if (e.keyCode != 8) {
+
+            //content attr is used for clearBoard() also.	
+            $(content).attr('dirty', true);
+            var isValidate = self.inputValidate(self.$cellMatrix, $(content).val(), $(content).data());
+            if (!isValidate) {
+                $(content).addClass('input-validate-false');
+            } else {
+                $(content).addClass('input-validate-true');
+            }
+        }
 
 
-		//if keyCode is 8, it means the user pressed Delete button
-		//We need to set the current input dirty to false again
-		//And we need to swap classes back
-		
-		if (e.keyCode === 8) {
-			$(content).attr('dirty', false);
-			$(content).removeClass('input-validate-false');
-			$(content).addClass('input-style');
-		}
-	};
+        /*If keyCode is 8, it means the user pressed Delete button
+        We need to set the current input dirty to false again
+        And we need to swap classes back*/
 
-	//Singleton public methods
-	return {
-		//get the Singleton instance
-		getInstance: function ( config ) {
-			if (!_instance) {
-				_instance = init( config );
-			}
-			return _instance;
-		}
-	};
-})( jQuery );
+        if (e.keyCode === 8) {
+            $(content).attr('dirty', false);
+            $(content).removeClass('input-validate-false');
+            $(content).removeClass('input-validate-true');
+            $(content).addClass('input-style');
+        }
+    };
+
+    //Singleton public methods
+    return {
+
+        //get the Singleton instance
+        getInstance: function() {
+            if (!_instance) {
+                _instance = init();
+            }
+            return _instance;
+        }
+    };
+})(jQuery);
