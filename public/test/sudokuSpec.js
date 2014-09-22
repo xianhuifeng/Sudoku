73ef575a8a3ef214c88fa22df2f2ab5893de09fd
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-21 23:34:22 -0700

    Add: Add Mocha test for Sudoku game, create public/test/sudokuSpec.js and public/test/test.jade.

---
+++ public/test/sudokuSpec.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;
+
+describe('testing', function() {
+	it('should show me something on the page', function () {
+		expect (true).to.equal(true);
+		console.log("here");
+	});
+});
