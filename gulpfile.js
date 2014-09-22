10cd658e3aa9c7fde666793a313888c844e80172
Xianhui Feng <xianhui.feng100@gmail.com>
2014-09-22 04:05:12 -0700

    Add: Gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var DEST = 'build/';
+
+gulp.task('default', function() {
+  return gulp.src('public/js/*.js')
+    .pipe(gulp.dest(DEST + "minified_js/"))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST + "minified_js/"));
+});
+
